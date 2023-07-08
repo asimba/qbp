d1c3b1595a695ccd733cedb66ebc33455415eea6
Alexey Simbarsky <asimbarsky@gmail.com>
2023-07-08 13:22:57 +0000

    Add files via upload

---
+++ build-qbp-miniwin32.cmd
@@ -0,0 +1,7 @@
+@echo off
+gcc -Os -s -static -m32 -mconsole -march=i686 -mtune=i686 -Wall ^
+-mpreferred-stack-boundary=2 -fomit-frame-pointer -fno-unwind-tables -fno-asynchronous-unwind-tables ^
+-fmerge-constants -fpack-struct=1 -falign-jumps=1 -falign-loops=1 -falign-functions=1 -nodefaultlibs ^
+-ffast-math -fno-stack-protector -fno-exceptions -fno-ident -nostartfiles ^
+-ffunction-sections -Wl,--entry=_start,--enable-stdcall-fixup,--no-insert-timestamp,--gc-sections ^
+-o qbp-miniwin32.exe qbp-miniwin32.c -lkernel32 -lshell32
